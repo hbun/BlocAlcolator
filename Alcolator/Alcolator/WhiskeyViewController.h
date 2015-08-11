a3df1f75a6d45be7b8f7d4de346668c1fed44008
Husam Al-Ziab <husam.alziab@gmail.com>
2015-08-11 13:38:57 -0700

    Completed checkpoint 20

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Husam Al-Ziab on 8/11/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
