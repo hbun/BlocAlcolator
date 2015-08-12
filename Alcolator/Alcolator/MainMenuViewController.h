20fc5c61cac5ee295e503ea9e4e0edf6fcff8792
Husam Al-Ziab <husam.alziab@gmail.com>
2015-08-11 23:00:15 -0700

    Checkpoint 21 complete, Navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Husam Al-Ziab on 8/11/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainMenuViewController : UIViewController
+
+@end
